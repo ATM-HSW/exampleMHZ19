24b1c49975e6c71c8cba88424e3937a23ab429bb
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2021-06-03 12:49:44 +0200

    adding calibration with user button to BasicUsage example

--- BasicUsage/BasicUsage.cpp
+++ BasicUsage/BasicUsage.cpp
@@ -8,15 +8,20 @@
   puts(""); \
 }
 
-MHZ19 myMHZ19;                                             // Constructor for library
-BufferedSerial rser(USBTX, USBRX);                       // (Uno example) create device to MH-Z19 serial
-BufferedSerial rserMHZ19(PC_12, PD_2);                       // (Uno example) create device to MH-Z19 serial
+MHZ19 myMHZ19;                             // Constructor for library
+BufferedSerial rser(USBTX, USBRX);         // Debug output serial
+BufferedSerial rserMHZ19(PC_12, PD_2);     // MH-Z19 serial
 
-
-unsigned long getDataTimer = 0;
+//#ifdef BUTTON1
+DigitalIn myBtn(BUTTON1);             // Calibration user button
+int btnvalue;  
+//#endif 
 
 int main() {
-  bool bStart = true;
+  bool bCalibrated = false;
+  
+  set_time(0);
+  btnvalue = myBtn.read();
 
   rser.set_baud(MBED_CONF_PLATFORM_STDIO_BAUD_RATE);           // Device to serial monitor feedback
 
@@ -31,7 +36,7 @@ int main() {
   myMHZ19.begin(rserMHZ19);                                // *Serial(Stream) refence must be passed to library begin(). 
   myMHZ19.printCommunication(false, false);
 
-  myMHZ19.autoCalibration();                               // Turn auto calibration ON (OFF autoCalibration(false))
+  myMHZ19.autoCalibration(false);                               // Turn auto calibration ON (OFF autoCalibration(false))
 
   char myVersion[4];          
   myMHZ19.getVersion(myVersion);
@@ -42,19 +47,38 @@ int main() {
   printf("Temperature Cal: %d\n", myMHZ19.getTempAdjustment());
   printf("ABC Status: %s\n", myMHZ19.getABC() ? "ON" : "OFF");
 
+  time_t oldseconds = time(NULL)-16;  // don't wait for first measurement
   while(true) {
     int CO2, ret; 
     int8_t Temp;
+    time_t seconds;
+    int h, m, s;
 
     /* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even 
     if below background CO2 levels or above range (useful to validate sensor). You can use the 
     usual documented command with getCO2(false) */
 
-    CO2 = myMHZ19.getCO2();                               // Request CO2 (as ppm)
-    Temp = myMHZ19.getTemperature();                      // Request Temperature (as Celsius)
-//    printf("CO2 (ppm): %d, Temperatur (C): %d\n", CO2, Temp);                                
-    printf("%d,%d\n", CO2, Temp);
+    seconds = time(NULL);
+    if(seconds - oldseconds > 15) {
+      CO2 = myMHZ19.getCO2();           // Request CO2 (as ppm)
+      Temp = myMHZ19.getTemperature();  // Request Temperature (as Celsius)
+      h = seconds/60/60;
+      m = (seconds-h*60*60)/60;
+      s = seconds-h*60*60-m*60;
+//      printf("Zeit: %03dh:%02dmin:%02ds, CO2 (ppm): %d, Temperatur (C): %d\n", h, m, s, CO2, Temp);                                
+      printf("%03dh:%02dmin:%02ds %d,%d\n", h, m, s, CO2, Temp);
+      oldseconds = seconds;
+    }
+    if(btnvalue != myBtn.read() && !bCalibrated) {
+      myMHZ19.calibrate();
+      printf("start calibration\n");
+      bCalibrated = true;
+      btnvalue = myBtn.read();
+    }
+    if(btnvalue != myBtn.read() && bCalibrated) {
+      btnvalue = myBtn.read();
+    }
 
-    thread_sleep_for(15000);
+    thread_sleep_for(100);
   }
 }
